fa77cddc3471c07e3d80f0c8aed5f292f036de26
GwerhDev <gwerh.dev@gmail.com>
2024-01-05 19:28:41 -0300

    adding inner login

--- src/app/components/LoginForm.vue
+++ src/app/components/LoginForm.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
+import { useStore } from '../../middlewares/store/index';
+import { useRouter } from 'vue-router';
+import { onMounted, computed } from 'vue';
+import { getUserToken } from '../../helpers';
 
+const store: any = useStore();
+const router: any = useRouter();
+const currentUser: any = computed(() => store.currentUser);
+
+let email = "";
+let password = "";
+let token = getUserToken();
+
+onMounted(() => {
+  if (!currentUser?.value.error && token) {
+    router.push('/account/settings/' + token);
+  }
+});
+
+async function handleInnerLogin(e: any) {
+  e.preventDefault();
+  const formData: any = { email, password };
+  try {
+    const path: any = await store.handleLogin(formData);
+    router.push(path);
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
 
 <template>
   <form class="login-form">
     <input type="text" placeholder="Correo electrónico o teléfono">
     <input type="password" placeholder="Contraseña">
-    <button type="button"><a href="http://localhost:8080/login-inner">Iniciar sesión</a></button>
+    <button type="button" @click="handleInnerLogin">Iniciar sesión</button>
   </form>
 </template>
 
