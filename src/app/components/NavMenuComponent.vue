153669028abc48ca25aac7512f5fc93ee9a97c7c
GwerhDev <gwerh.dev@gmail.com>
2024-01-31 17:58:57 -0300

    styling

--- src/app/components/NavMenuComponent.vue
+++ src/app/components/NavMenuComponent.vue
@@ -7,6 +7,8 @@
   <ul>
     <router-link to='/home'><li class="nav-button">Inicio</li></router-link>
     <router-link to='/my-tasks'><li class="nav-button">Mis Tareas</li></router-link>
+    <router-link to='/calendar'><li class="nav-button">Calendario</li></router-link>
     <router-link to='/gallery'><li class="nav-button">Galería</li></router-link>
+    <router-link to='/guides'><li class="nav-button">Guías</li></router-link>
   </ul>
 </template>
