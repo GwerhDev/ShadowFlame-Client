b8064f3e4647e41138d5628b4d3bfd384f5bf3d8
GwerhDev <gwerh.dev@gmail.com>
2024-02-07 09:38:07 -0300

    styling

--- src/app/pages/TasksPage.vue
+++ src/app/pages/TasksPage.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import ToDoComponent from '../components/todo/ToDoComponent.vue';
-
 </script>
 
 <template>
   <main class="main-container red-shadow-fx">
-    <ToDoComponent />
+    <div class="div-container">
+      <ToDoComponent />
+    </div>
   </main>
 </template>
 
 <style scoped>
-
+.div-container {
+  padding-left: 1.3rem;
+  padding-right: 1.3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
 
