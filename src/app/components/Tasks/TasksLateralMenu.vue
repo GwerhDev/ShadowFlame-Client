91aae43b1dc228cc7efed413fb4466f173b14088
GwerhDev <gwerh.dev@gmail.com>
2024-02-15 18:07:19 -0300

    styling

--- src/app/components/Tasks/TasksLateralMenu.vue
+++ src/app/components/Tasks/TasksLateralMenu.vue
@@ -1,13 +1,21 @@
 <style scoped lang="scss" src="./TasksLateralMenu.scss"/>
 <script setup lang="ts">
+import { Ref, ref } from 'vue';
 import { useStore } from '../../../middlewares/store';
 
 const store: any = useStore();
 const props: any = defineProps({ logged: Boolean });
+const active: Ref = ref({ backgroundColor: "var(--color-primary)" })
 
 function handleType(type: string) {
   store.setTaskType(type);
   store.handleGetTask(store.currentUser.taskdate, store.currentUser.tasktype);
+};
+
+function styleActive(type: string) {
+  if (store.currentUser.tasktype === type) {
+    return active.value;
+  };
 };
 
 </script>
@@ -16,14 +24,14 @@ function handleType(type: string) {
   <div class="container-lateral">
     <ul>
       <li>
-        <button :disabled="!props.logged" class="first" @click="handleType('mytasks')">
+        <button :disabled="!props.logged" class="first" @click="handleType('mytasks')" :style="styleActive('mytasks')">
           <img src="../../../assets/svg/profile-icon.svg" alt="" />
           <span>Mis Tareas</span>
           <span></span>
         </button>
       </li>
       <li>
-        <button :disabled="!props.logged" @click="handleType('clantasks')">
+        <button :disabled="!props.logged" @click="handleType('clantasks')" :style="styleActive('clantasks')">
           <img src="../../../assets/svg/clan-shield-icon.svg" alt="" />
           <span>Clan</span>
           <span></span>
