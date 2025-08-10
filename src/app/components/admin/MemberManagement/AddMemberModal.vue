1f86a97dd376dce206bed88b6dba43f3807138d5
GwerhDev <gwerh.dev@gmail.com>
2025-08-10 02:48:47 -0400

    styling

--- src/app/components/admin/MemberManagement/AddMemberModal.vue
+++ src/app/components/admin/MemberManagement/AddMemberModal.vue
@@ -8,8 +8,24 @@ const emit = defineEmits(['close']);
 const username = ref('');
 const character = ref('');
 const resonance = ref(0);
-const memberClass = ref('');
+const selectedClass = ref('');
 const whatsapp = ref('');
+
+const classes = [
+  { name: "Druida", value: "druid", image: "/dummy-druid.png" },
+  { name: "Bárbaro", value: "barbarian", image: "/dummy-barbarian.png" },
+  { name: "Caballero de Sangre", value: "bloodknight", image: "/dummy-bloodknight.png" },
+  { name: "Guerrero Divino", value: "crusader", image: "/dummy-crusader.png" },
+  { name: "Cazador de Demonios", value: "demonhunter", image: "/dummy-demonhunter.png" },
+  { name: "Monje", value: "monk", image: "/dummy-monk.png" },
+  { name: "Nigromante", value: "necromancer", image: "/dummy-necromancer.png" },
+  { name: "Tempest", value: "tempest", image: "/dummy-tempest.png" },
+  { name: "Arcanista", value: "wizard", image: "/dummy-wizard.png" },
+];
+
+function handleClassSelection(classValue: string) {
+  selectedClass.value = classValue;
+};
 
 function handleCloseModal() {
   emit('close');
@@ -21,7 +37,7 @@ function handleSubmit() {
     username: username.value,
     character: character.value,
     resonance: resonance.value,
-    class: memberClass.value,
+    class: selectedClass.value,
     whatsapp: whatsapp.value,
   };
   console.log('New Member Data:', formData);
@@ -50,15 +66,21 @@ function handleSubmit() {
               </span>
               <span class="d-flex g-1">
                 <label for="resonance">Resonance:</label>
-                <input type="number" id="resonance" v-model.number="resonance">
-              </span>
-              <span class="d-flex g-1">
-                <label for="class">Class:</label>
-                <input type="text" id="class" v-model="memberClass" required>
+                <input type="number" id="resonance" v-model.number="resonance" min="0">
               </span>
               <span class="d-flex g-1">
                 <label for="whatsapp">WhatsApp:</label>
                 <input type="text" id="whatsapp" v-model="whatsapp">
+              </span>
+              <span class="d-flex col g-1">
+                <label>Class:</label>
+                <div class="class-selection d-flex row g-1">
+                  <button type="button" v-for="cls in classes" :key="cls.value" @click="handleClassSelection(cls.value)"
+                    :class="{ 'selected-class': selectedClass === cls.value }">
+                    <img :src="cls.image" :alt="cls.name" width="50" height="50">
+                    <span>{{ cls.name }}</span>
+                  </button>
+                </div>
               </span>
               <button type="submit"
                 class="submit-button button justify-content-center align-items-center d-flex g-1 w-100">Add
