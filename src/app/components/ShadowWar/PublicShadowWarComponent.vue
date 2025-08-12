42f0747c1f19a91a98b02b3be88a3a7f8c4f7497
GwerhDev <gwerh.dev@gmail.com>
2025-08-11 23:26:43 -0400

    styling

--- src/app/components/ShadowWar/PublicShadowWarComponent.vue
+++ src/app/components/ShadowWar/PublicShadowWarComponent.vue
@@ -1,16 +1,24 @@
 <style scoped lang="scss" src="./PublicShadowWarComponent.scss" />
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useStore } from '../../../middlewares/store';
 import ShadowWarLateralMenu from './PublicShadowWarLateralMenu.vue';
 import PublicShadowWar from './PublicShadowWar.vue';
 import diabloIcon from "../../../assets/svg/diablo-icon.svg";
 
 const store: any = useStore();
+const nextWarDate = ref('');
+const warTime = ref('');
 
-onMounted(() => {
-  store.handleGetNextShadowWar();
+onMounted(async () => {
+  await store.handleGetNextShadowWar();
+  if (store.currentUser.shadowWarData && store.currentUser.shadowWarData.date) {
+    const warDate = new Date(store.currentUser.shadowWarData.date);
+    nextWarDate.value = warDate.toLocaleString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    warTime.value = warDate.toLocaleString('es-ES', { hour: '2-digit', minute: '2-digit' });
+  }
 });
+
 </script>
 
 <template>
@@ -20,6 +28,7 @@ onMounted(() => {
         <img :src="diabloIcon" alt="icon" />
         <h1>Guerra Sombría</h1>
       </span>
+      <p>La próxima Guerra Sombría es el {{ nextWarDate }} a las {{ warTime }}h (hora del servidor).</p>
 
       <div class="section-container">
         <section class="menu-section">
