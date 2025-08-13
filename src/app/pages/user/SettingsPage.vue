121e696d4b90472f6bf9813966db72a793270bef
gerardo <gerardo@recemed.com>
2025-08-13 19:22:12 -0400

    styling

--- src/app/pages/user/SettingsPage.vue
+++ src/app/pages/user/SettingsPage.vue
@@ -12,3 +12,21 @@ import SettingsComponent from '../../components/Settings/SettingsComponent.vue';
     </div>
   </main>
 </template>
+
+<style scoped>
+
+.div-container {
+  height: 100%;
+  padding: 1rem;
+  padding-top: 7rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media (max-width: 1100px) {
+  .div-container {
+    padding: 0;
+  }
+}
+</style>
