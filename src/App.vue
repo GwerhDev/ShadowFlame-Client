aa2b8e8c6a0c97a012fc50b954d33bc58be12912
gerardo <gerardo@recemed.com>
2025-08-14 11:53:40 -0400

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<script setup lang="ts"> 
-  import { useStore } from './middlewares/store/index';
-  import { Ref, ref, onMounted } from 'vue';
-  import NavComponent from './app/components/NavComponent.vue';
+<script setup lang="ts">
+import { useStore } from './middlewares/store/index';
+import { Ref, ref, onMounted } from 'vue';
+import NavComponent from './app/components/NavComponent.vue';
 
-  const store: any = useStore();
-  const token: any = localStorage.getItem('userToken');
-  const loggedin: Ref = ref(false);
+const store: any = useStore();
+const token: any = localStorage.getItem('userToken');
+const loggedin: Ref = ref(false);
 
-  onMounted(async() => {
+onMounted(async () => {
   try {
     const response = await store.handleUserData(token);
-    if(response) {
+    if (response) {
       loggedin.value = true;
     }
   } catch (error) {
@@ -20,11 +20,12 @@
   }
 });
 
-</script> 
- 
+</script>
+
 <template>
-  <router-view/>
-  <template v-if="!$route.path.startsWith('/signup') && !$route.path.startsWith('/login') && !$route.path.startsWith('/auth')">
-    <nav-component :loggedin="loggedin"/> 
+  <router-view />
+  <template
+    v-if="!$route.path.startsWith('/signup') && !$route.path.startsWith('/login') && !$route.path.startsWith('/auth')">
+    <nav-component :loggedin="loggedin" />
   </template>
 </template>
